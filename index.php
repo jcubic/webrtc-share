dc6a6a6fb9c7602288ce5ce8cce92deea2efbc11
Jakub Jankiewicz <jcubic@onet.pl>
2019-10-10 19:26:45 +0200

    copyright note in index.php file

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
+/*  This file is part of WebRTC Share (send.jcubic.pl)
+ *  Application code for WebRTC/Firebase based P2P file sharing app
+ *
+ *  Copyright (C) Jakub T. Jankiewicz <https://jcubic.pl>
+ *
+ *  WebRTC Share is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *
+ * MIT licensed POC can be found at https://codepen.io/jcubic/pen/yvMeRg
+ *
+ */
 
 if (!isset($_GET['room']) && empty($_GET['room'])) {
     $url = self_url();
